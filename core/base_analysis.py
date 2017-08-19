2ad034748c65691dbacdc50445ed0c0686e392a2
dkruglov <dkruglov@internal.oculus-tech.com>
2017-08-19 16:31:30 +0300

    Add debug for comparison methods

--- core/base_analysis.py
+++ core/base_analysis.py
@@ -72,12 +72,13 @@ def get_diff_lowest_ask_vs_highest_bid(first_one, second_one, threshold):
     difference = get_change(first_one.ask, second_one.bid)
 
     if should_print_debug():
-        print "ASK: ", first_one.ask
+        print "get_diff_lowest_ask_vs_highest_bid"
+	print "ASK: ", first_one.ask
         print "BID: ", second_one.bid
         print "DIFF: ", difference
 
     if difference >= threshold:
-        msg = "Lowest ask differ from highest bid more than {num}".format(num=threshold)
+        msg = "Lowest ask differ from highest bid more than {num} %".format(num=threshold)
         return msg, first_one.pair_id, first_one, second_one
 
     return ()
@@ -86,12 +87,13 @@ def check_highest_bid_bigger_than_lowest_ask(first_one, second_one, threshold):
     difference = get_change(first_one.bid, second_one.ask, provide_abs = False)
 
     if should_print_debug():
-        print "ASK: ", first_one.ask
+        print "check_highest_bid_bigger_than_lowest_ask"
+	print "ASK: ", first_one.ask
         print "BID: ", second_one.bid
         print "DIFF: ", difference
 
     if difference >= threshold:
-        msg = "highest bid bigger than Lowest ask for more than {num}".format(num=threshold)
+        msg = "highest bid bigger than Lowest ask for more than {num} %".format(num=threshold)
         return msg, first_one.pair_id, first_one, second_one
 
     return ()
