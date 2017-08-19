c5cbf850f7e6be4f669dadc1cd5ecfc95fb5058e
dkruglov <dkruglov@internal.oculus-tech.com>
2017-08-19 16:11:38 +0300

    modification for file parsing to deal with gigabytes of data. FIXME note: use copy for postgres instead

--- file_parsing.py
+++ file_parsing.py
@@ -53,9 +53,16 @@ def constructor_selector(class_name, string_repr):
 
 def load_data_from_file(every_file, pattern_name):
     array = []
+    dummy_flag = (pattern_name == ORDER_BOOK_TYPE_NAME)
     with open(every_file, "r") as ins:
         for line in ins:
-            array.append(constructor_selector(pattern_name, line))
+        	obj = constructor_selector(pattern_name, line)
+		insert_data(obj, pg_conn, dummy_flag)
+    		pg_conn.commit()
+		#     array.append(constructor_selector(pattern_name, line))
+		#     if len(array) >= 100000:
+        	#     	load_to_postgres(array, pattern_name, pg_conn)
+		# 	array = []
     return array
 
 def load_to_postgres(array, pattern_name, pg_conn):
@@ -72,7 +79,7 @@ def load_crap_from_folder(folder_name, pattern_name, pg_conn):
     for every_file in file_list:
 	print "Processing file ", every_file
         array = load_data_from_file(every_file, pattern_name)
-        load_to_postgres(array, pattern_name, pg_conn)
+        # load_to_postgres(array, pattern_name, pg_conn)
 
 def init_pg_connection():
     # FIXME NOTE hardcoding is baaad Dmitry! pass some config
@@ -86,8 +93,8 @@ if __name__ == "__main__":
 
     pg_conn = init_pg_connection()
 
-    # file_name_patterns = [TICKER_TYPE_NAME, CANDLE_TYPE_NAME, ORDER_BOOK_TYPE_NAME, TRADE_HISTORY_TYPE_NAME]
-    file_name_patterns = [ORDER_BOOK_TYPE_NAME]
+    file_name_patterns = [TICKER_TYPE_NAME, CANDLE_TYPE_NAME, TRADE_HISTORY_TYPE_NAME]
+    # file_name_patterns = [ORDER_BOOK_TYPE_NAME]
 
     for every_pattern in file_name_patterns:
         load_crap_from_folder(folder_name, every_pattern, pg_conn)
