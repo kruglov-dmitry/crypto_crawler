5d20f66e867b89a5c76024bf2a5190af67619fc6
Dmitry <dkruglov@internal.oculus-tech.com>
2017-09-12 12:11:24 +0400

    Add wrapper for bittrex to perform market deals

--- bittrex/constants.py
+++ bittrex/constants.py
@@ -14,3 +14,12 @@ BITTREX_GET_HISTORY = "https://bittrex.com/api/v1.1/public/getmarkethistory?mark
 
 BITTREX_CURRENCIES = ["BTC-DASH", "BTC-ETH", "BTC-LTC", "BTC-XRP", "BTC-BCC", "BTC-ETC", "BTC-SC", "BTC-DGB",
                       "BTC-XEM", "BTC-ARDR"]
+
+# https://bittrex.com/api/v1.1/market/buylimit?apikey=API_KEY&market=BTC-LTC&quantity=1.2&rate=1.3
+BITTREX_BUY_ORDER = "https://bittrex.com/api/v1.1/market/buylimit?apikey="
+
+# https://bittrex.com/api/v1.1/market/selllimit?apikey=API_KEY&market=BTC-LTC&quantity=1.2&rate=1.3    
+BITTREX_SELL_ORDER = "https://bittrex.com/api/v1.1/market/selllimit?apikey="
+
+# https://bittrex.com/api/v1.1/market/cancel?apikey=API_KEY&uuid=ORDER_UUID
+BITTREX_CANCEL_ORDER = "https://bittrex.com/api/v1.1/market/cancel?apikey="
---
+++ bittrex/market_utils.py
@@ -0,0 +1,43 @@
+from constants import BITTREX_CANCEL_ORDER, BITTREX_BUY_ORDER, BITTREX_SELL_ORDER
+from data_access.internet import send_request
+
+
+def add_buy_order(key, pair_name, price, amount):
+	# https://bittrex.com/api/v1.1/market/buylimit?apikey=API_KEY&market=BTC-LTC&quantity=1.2&rate=1.3
+    	final_url = BITTREX_BUY_ORDER + key + "&market=" + pair_name + "&quantity=" + str(amount) + "&rate=" + str(price)
+
+    	if should_print_debug():
+        	print final_url
+
+	err_msg = "add_buy_order bittrex called for {pair} for amount = {amount} with price {price}".format(pair=pair_name, amount=amount, price=price)
+	r = send_request(final_url, err_msg)
+
+	# FIXME
+	print r
+
+def add_sell_order(key, pair_name, price, amount):
+	# https://bittrex.com/api/v1.1/market/selllimit?apikey=API_KEY&market=BTC-LTC&quantity=1.2&rate=1.3
+	final_url = BITTREX_SELL_ORDER + key + "&market=" + pair_name + "&quantity=" + str(amount) + "&rate=" + str(price)
+
+    	if should_print_debug():
+        	print final_url
+
+	err_msg = "add_sell_order bittrex called for {pair} for amount = {amount} with price {price}".format(pair=pair_name, amount=amount, price=price)
+	r = send_request(final_url, err_msg)
+
+	# FIXME
+	print r
+
+	
+def cancel_order(key, deal_id):
+	# https://bittrex.com/api/v1.1/market/cancel?apikey=API_KEY&uuid=ORDER_UUID
+	final_url = BITTREX_CANCEL_ORDER + key + "&uuid=" + deal_id
+
+    	if should_print_debug():
+        	print final_url
+
+	err_msg = "cancel bittrex order with id {id}".format(id=deal_id)
+	r = send_request(final_url, err_msg)
+
+	# FIXME
+	print r
