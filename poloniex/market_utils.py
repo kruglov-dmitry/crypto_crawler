7cbddb700d6b151dc6f80a28f67311a59dbd8429
Dmitry <kruglov.dima@gmail.com>
2017-09-27 21:21:18 +0400

    move balance check to poloniex market utils

--- poloniex/market_utils.py
+++ poloniex/market_utils.py
@@ -2,7 +2,6 @@ from constants import POLONIEX_CANCEL_ORDER, POLONIEX_BUY_ORDER, POLONIEX_SELL_O
 from data_access.internet import send_post_request_with_header
 import time
 from debug_utils import should_print_debug
-from utils.key_utils import get_key_by_exchange
 import hmac
 import hashlib
 from urllib import urlencode as _urlencode
@@ -15,15 +14,14 @@ def generate_nonce():
     return int(round(time.time() * 1000))
 
 
-def signed_body(body):
-    key = get_key_by_exchange("poloniex")
+def signed_body(body, secret):
     #  The query's POST data signed by your key's "secret" according to the HMAC-SHA512 method.
-    payload = hmac.new(key.secret, _urlencode(body), hashlib.sha512).hexdigest()
+    payload = hmac.new(secret, _urlencode(body), hashlib.sha512).hexdigest()
 
     return payload
 
 
-def add_buy_order_poloniex(key, pair_name, price, amount):
+def add_buy_order_poloniex(api_key, pair_name, price, amount):
     body = {
         "command": "buy",
         "currencyPair": pair_name,
@@ -32,7 +30,7 @@ def add_buy_order_poloniex(key, pair_name, price, amount):
         "nonce": generate_nonce()
     }
 
-    headers = {"Key": key, "Sign": signed_body(body)}
+    headers = {"Key": api_key.key, "Sign": signed_body(body, api_key.secret)}
     # https://poloniex.com/tradingApi
     final_url = POLONIEX_BUY_ORDER
 
@@ -45,7 +43,7 @@ def add_buy_order_poloniex(key, pair_name, price, amount):
     print r
 
 
-def add_sell_order_poloniex(key, pair_name, price, amount):
+def add_sell_order_poloniex(api_key, pair_name, price, amount):
     body = {
         "command": "sell",
         "currencyPair": pair_name,
@@ -54,7 +52,7 @@ def add_sell_order_poloniex(key, pair_name, price, amount):
         "nonce": generate_nonce()
     }
 
-    headers = {"Key": key, "Sign": signed_body(body)}
+    headers = {"Key": api_key.key, "Sign": signed_body(body, api_key.secret)}
 
     # https://poloniex.com/tradingApi
     final_url = POLONIEX_SELL_ORDER
@@ -68,14 +66,14 @@ def add_sell_order_poloniex(key, pair_name, price, amount):
     print r
 
 
-def cancel_order_poloniex(key, deal_id):
+def cancel_order_poloniex(api_key, deal_id):
     body = {
         "command": "cancelOrder",
         "orderNumber" : deal_id,
         "nonce": generate_nonce()
     }
 
-    headers = {"Key": key, "Sign": signed_body(body)}
+    headers = {"Key": api_key.key, "Sign": signed_body(body, api_key.secret)}
 
     # https://poloniex.com/tradingApi
     final_url = POLONIEX_CANCEL_ORDER
@@ -84,6 +82,18 @@ def cancel_order_poloniex(key, deal_id):
         print final_url, headers, body
 
     err_msg = "cancel poloniex called for {deal_id}".format(deal_id=deal_id)
+
+    r = send_post_request_with_header(final_url, headers, body, err_msg)
+    print r
+
+
+def show_balance_poloniex(api_key):
+    body = {
+        'command': 'returnBalances',
+        'nonce': int(time.time() * 1000)
+    }
+
+    headers = {"Key": api_key.key, "Sign": signed_body(body, api_key.secret)}
 
     r = send_post_request_with_header(final_url, headers, body, err_msg)
     print r
