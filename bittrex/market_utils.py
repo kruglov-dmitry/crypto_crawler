f7c50bf4013e8f7340fc90ec6bb2721470c32aad
Dmitry <kruglov.dima@gmail.com>
2017-09-27 22:57:12 +0400

    Update buy\sell\cancel methods for bittrex

--- bittrex/market_utils.py
+++ bittrex/market_utils.py
@@ -1,23 +1,23 @@
 from constants import BITTREX_CANCEL_ORDER, BITTREX_BUY_ORDER, BITTREX_SELL_ORDER, BITTREX_CHECK_BALANCE
 from debug_utils import should_print_debug
-from utils.key_utils import signed_string, generate_nonce, signed_body
+from utils.key_utils import signed_string, generate_nonce
 from data_access.internet import send_post_request_with_header
+from urllib import urlencode as _urlencode
 
 
 def add_buy_order_bittrex(key, pair_name, price, amount):
     # https://bittrex.com/api/v1.1/market/buylimit?apikey=API_KEY&market=BTC-LTC&quantity=1.2&rate=1.3
-    final_url = BITTREX_BUY_ORDER # \
-    # + key + "&market=" + pair_name + "&quantity=" + str(amount) + "&rate=" + str(price)
+    final_url = BITTREX_BUY_ORDER + key.api_key + "&nonce=" + generate_nonce()
 
     body = {
-        "apikey": key.api_key,
         "market": pair_name,
         "quantity": amount,
-        "rate": price,
-        "nonce": generate_nonce()
+        "rate": price
     }
 
-    headers = {"apisign": signed_body(body, key.secret)}
+    final_url += _urlencode(body)
+
+    headers = {"apisign": signed_string(final_url, key.secret)}
 
     if should_print_debug():
         print final_url, headers, body
@@ -30,17 +30,17 @@ def add_buy_order_bittrex(key, pair_name, price, amount):
 
 def add_sell_order_bittrex(key, pair_name, price, amount):
     # https://bittrex.com/api/v1.1/market/selllimit?apikey=API_KEY&market=BTC-LTC&quantity=1.2&rate=1.3
-    final_url = BITTREX_SELL_ORDER # + key + "&market=" + pair_name + "&quantity=" + str(amount) + "&rate=" + str(price)
+    final_url = BITTREX_SELL_ORDER + key.api_key + "&nonce=" + generate_nonce()
 
     body = {
-        "apikey": key.api_key,
         "market": pair_name,
         "quantity": amount,
-        "rate": price,
-        "nonce": generate_nonce()
+        "rate": price
     }
 
-    headers = {"apisign": signed_body(body, key.secret)}
+    final_url += _urlencode(body)
+
+    headers = {"apisign": signed_string(final_url, key.secret)}
 
     if should_print_debug():
         print final_url, headers, body
@@ -53,15 +53,15 @@ def add_sell_order_bittrex(key, pair_name, price, amount):
 
 def cancel_order_bittrex(key, deal_id):
     # https://bittrex.com/api/v1.1/market/cancel?apikey=API_KEY&uuid=ORDER_UUID
-    final_url = BITTREX_CANCEL_ORDER # + key + "&uuid=" + deal_id
+    final_url = BITTREX_CANCEL_ORDER + key.api_key + "&nonce=" + generate_nonce()
 
     body = {
-        "apikey": key.api_key,
         "uuid": deal_id,
-        "nonce": generate_nonce()
     }
 
-    headers = {"apisign": signed_body(body.encode(), key.secret)}
+    final_url += _urlencode(body)
+
+    headers = {"apisign": signed_string(final_url, key.secret)}
 
     if should_print_debug():
         print final_url, headers, body
@@ -73,11 +73,13 @@ def cancel_order_bittrex(key, deal_id):
 
 
 def show_balance_bittrex(key):
-    # https://poloniex.com/tradingApi
+
     final_url = BITTREX_CHECK_BALANCE + key.api_key + "&nonce=" + str(generate_nonce())
 
     body = {
     }
+
+    final_url += _urlencode(body)
 
     print final_url, type(final_url)
 
