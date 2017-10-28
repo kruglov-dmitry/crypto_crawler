231b6b026a5d964bca16ff80bcba7ee5c1eb6bbb
dkruglov <dkruglov@internal.oculus-tech.com>
2017-10-28 17:11:10 +0300

    change order for time entries to be ascending, bump deals limit to 1000

--- arbitrage_bot.py
+++ arbitrage_bot.py
@@ -307,6 +307,7 @@ def run_analysis_over_db(deal_threshold, balance_adjust_threshold, treshold_reve
         print current_balance.balance_per_exchange[exch_id]
 
     for every_time_entry in time_entries:
+        print every_time_entry
         order_book_grouped_by_time = get_order_book_by_time(pg_conn, every_time_entry)
 
         # for x in order_book_grouped_by_time:
@@ -318,7 +319,8 @@ def run_analysis_over_db(deal_threshold, balance_adjust_threshold, treshold_reve
         cnt += 1
         print "Processed ", cnt, " out of ", time_entries_num, " time entries"
         print current_balance
-        raise
+        if cnt == 1000:
+            raise
 
     print "At the end of processing we have following balance:"
     print "NOTE: supposedly all buy \ sell request were fullfilled"
