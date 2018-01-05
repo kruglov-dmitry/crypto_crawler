1442387969afee226ccb5a62501e00ac681b42d5
Dmitry <kruglov.dima@gmail.com>
2018-01-05 10:36:17 +0400

    Fix failed tg message reprocessing logic

--- services/telegram_notifier.py
+++ services/telegram_notifier.py
@@ -20,13 +20,11 @@ if __name__ == "__main__":
                 do_we_have_data = True
                 notification_id = get_notification_id_by_topic_name(topic_id)
                 err_code = send_single_message(msg, notification_id)
-                if err_code != STATUS.SUCCESS:
-                    err_msg = """telegram_notifier can't send message to telegram. Message will be saved to file only.
+                if err_code == STATUS.FAILURE:
+                    err_msg = """telegram_notifier can't send message to telegram. Message will be re-processed on next iteration.
                     {msg}""".format(msg=msg)
                     log_to_file(err_msg, "telegram_notifier.log")
                     print_to_console(err_msg, LOG_ALL_ERRORS)
-                else:
-                    print_to_console("Message sent FAILED! {msg}\n Check telegram.log for details.".format(msg=msg), LOG_ALL_ERRORS)
                     msg_queue.add_message_to_start(topic_id, msg)
                     sleep_for(1)
 
