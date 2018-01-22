4cec876888ac9ef773b55ee4466c522890d5a199
EC2 Default User <ec2-user@ip-172-31-31-75.us-west-1.compute.internal>
2018-01-22 17:27:45 +0000

    Save aws-related config

--- deploy/update_nonce_redis.py
+++ deploy/update_nonce_redis.py
@@ -4,15 +4,17 @@ from dao.db import get_arbitrage_id, init_pg_connection
 
 
 def update_nonce():
-    r = _redis.StrictRedis(host='192.168.1.106', port=6379, db=0)
+    r = _redis.StrictRedis(host='54.193.19.230', port=6379, db=0)
     ts = int(round(time.time() * 1000))
     r.set('nonce', str(ts))
 
 
 def update_arbitrage_id():
-    r = _redis.StrictRedis(host='192.168.1.106', port=6379, db=0)
+    r = _redis.StrictRedis(host='54.193.19.230', port=6379, db=0)
 
-    pg_conn = init_pg_connection()
+    # pg_conn = init_pg_connection()
+    pg_conn = init_pg_connection(_db_host="orders.cervsj06c8zw.us-west-1.rds.amazonaws.com",
+                                 _db_port=5432, _db_name="crypto")
     next_arbitrage_id = get_arbitrage_id(pg_conn)
     r.set('arbitrage_id', str(next_arbitrage_id))
 
