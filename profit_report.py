794a0b67526bfccc8ee92b311dc3459e898c4558
Dmitry <kruglov.dima@gmail.com>
2018-01-29 08:17:53 +0400

    #40 add loss calculation for orders with missing paired arbitrage orders

--- profit_report.py
+++ profit_report.py
@@ -18,75 +18,25 @@ from analysis.data_load_for_profit_report import fetch_trades_history_to_db
 from analysis.binance_order_by_trades import group_binance_trades_per_order
 
 
-def group_by_pair_and_arbitrage_id(order_list):
-    res = defaultdict(list)
-
-    tmp = defaultdict(list)
-    for entry in order_list:
-        tmp[str(entry.volume)].append(entry)
-
-    for arbitrage_id in tmp:
-        if len(tmp[arbitrage_id]) != 2:
-            log_to_file("NOT FOUND deals for volume {a_id}".format(a_id=arbitrage_id),
-                        "what_we_have_at_the_end.log")
-        else:
-            deal_1, deal_2 = tmp[arbitrage_id]
-            if deal_1.trade_type == DEAL_TYPE.SELL:
-                res[deal_1.pair_id].append((deal_1, deal_2))
-            else:
-                res[deal_1.pair_id].append((deal_2, deal_1))
-
-    return res
-
-
-def find_corresponding_trades(deal_from_bot, trade_history):
-    res = []
-    tot_volume = 0.0
-    if deal_from_bot.exchange_id in [EXCHANGE.BITTREX, EXCHANGE.POLONIEX]:
-        if deal_from_bot.pair_id in trade_history:
-            res = [x for x in trade_history[deal_from_bot.pair_id] if x.deal_id == deal_from_bot.deal_id]
-        else:
-            log_to_file("NOT FOUND deal in history for {a_id}".format(a_id=deal_from_bot),
-                        "what_we_have_at_the_end.log")
-    elif deal_from_bot.exchange_id == EXCHANGE.BINANCE:
-        if deal_from_bot.pair_id in trade_history:
-            for trade in trade_history[deal_from_bot.pair_id]:
-                if trade.trade_type == deal_from_bot.trade_type and 0 < deal_from_bot.execute_time - trade.execute_time < 2 \
-                        and deal_from_bot.volume >= tot_volume:
-                    tot_volume += trade.volume
-                    res.append(trade)
-
-        if len(res) == 0:
-            log_to_file("NOT FOUND deal in history for {a_id}".format(a_id=deal_from_bot),
-                        "what_we_have_at_the_end.log")
-    else:
-        raise
-
-    return res
-
-
-def compute_profit_by_arbitrage(buy_deal, deal_1_trades, sell_deal, deal_2_trades):
+def compute_profit_by_arbitrage(buy_order, buy_trades, sell_order, sell_trades):
     """
 
                 NOTE: pair of deal_1 & deal_2 should be already in proper direction
 
-    :param buy_deal:
-    :param sell_deal:
+    :param buy_order:
+    :param sell_order:
     :param trades_exchange_1:
     :param trades_exchange_2:
     :return:
     """
 
     # This is how we approximate profit for TG messaging
-    expected_profit = buy_deal.volume * buy_deal.price * 0.01 * (100 - get_fee_by_exchange(buy_deal.exchange_id)) - \
-                      sell_deal.volume * sell_deal.price * 0.01 * (100 + get_fee_by_exchange(sell_deal.exchange_id))
-
-    """deal_1_trades = find_corresponding_trades(deal_1, trades_exchange_1)
-    deal_2_trades = find_corresponding_trades(deal_2, trades_exchange_2)"""
+    expected_profit = buy_order.volume * buy_order.price * 0.01 * (100 - get_fee_by_exchange(buy_order.exchange_id)) - \
+                      sell_order.volume * sell_order.price * 0.01 * (100 + get_fee_by_exchange(sell_order.exchange_id))
 
     actual_profit = 0.0
     # tot_volume = 0.0
-    for b in deal_1_trades:
+    for b in buy_trades:
         actual_profit += b.volume * b.price * 0.01 * (100 - get_fee_by_exchange(b.exchange_id))
         """ tot_volume += b.volume
         if tot_volume > deal_1.volume:
@@ -98,7 +48,7 @@ def compute_profit_by_arbitrage(buy_deal, deal_1_trades, sell_deal, deal_2_trade
         """
 
     # tot_volume = 0.0
-    for b in deal_2_trades:
+    for b in sell_trades:
         actual_profit = actual_profit - b.volume * b.price * 0.01 * (100 + get_fee_by_exchange(b.exchange_id))
         """tot_volume += b.volume
         if tot_volume > deal_2.volume:
@@ -113,22 +63,6 @@ def compute_profit_by_arbitrage(buy_deal, deal_1_trades, sell_deal, deal_2_trade
     return actual_profit
 
 
-def group_by_pair_and_exchange_id(history_orders):
-    poloniex_orders_by_pair = defaultdict(list)
-    bittrex_orders_by_pair = defaultdict(list)
-    binance_orders_by_pair = defaultdict(list)
-
-    for entry in history_orders:
-        if entry.exchange_id == EXCHANGE.POLONIEX:
-            poloniex_orders_by_pair[entry.pair_id].append(entry)
-        elif entry.exchange_id == EXCHANGE.BITTREX:
-            bittrex_orders_by_pair[entry.pair_id].append(entry)
-        elif entry.exchange_id == EXCHANGE.BINANCE:
-            binance_orders_by_pair[entry.pair_id].append(entry)
-
-    return binance_orders_by_pair, bittrex_orders_by_pair, poloniex_orders_by_pair
-
-
 def group_by_pair_id(binance_trades):
     res = defaultdict(list)
     for entry in binance_trades:
@@ -136,28 +70,18 @@ def group_by_pair_id(binance_trades):
     return res
 
 
-def compute_time_key(timest, rounding_interval):
-    return rounding_interval * long(timest / rounding_interval)
-
-
 def compute_profit_by_pair(trades_to_order_by_pair):
     profit_by_pair = 0.0
 
-    orders_by_volume = defaultdict(list)
+    orders_by_arbitrage_id = defaultdict(list)
 
     # 1 stage group by arbitrage id
     for order, trades in trades_to_order_by_pair:
-        orders_by_volume[order.volume].append( (order, trades) )
+        orders_by_arbitrage_id[order.arbitrage_id].append( (order, trades) )
 
-    cnt = 0
-    for arbitrage_id in orders_by_volume:
-        if len(orders_by_volume[arbitrage_id]) == 1:
-            for order, ll in orders_by_volume[arbitrage_id]:
-                msg = "can't find pair order - {o}".format(o=order)
-                log_to_file(msg, "missing_" + get_pair_name_by_id(order.pair_id) + ".txt")
-                cnt += 1
-        elif len(orders_by_volume[arbitrage_id]) > 2:
-
+    number_of_missing_pair = 0
+    for arbitrage_id in orders_by_arbitrage_id:
+        if len(orders_by_arbitrage_id[arbitrage_id]) > 2:
             def find_candidates(order, some_list):
                 # pair_id and volume the same
                 res = []
@@ -179,17 +103,17 @@ def compute_profit_by_pair(trades_to_order_by_pair):
 
             deal_ids = defaultdict(list)
 
-            list_of_orders = [x for x,y in orders_by_volume[arbitrage_id]]
+            list_of_orders = [x for x,y in orders_by_arbitrage_id[arbitrage_id]]
 
             trades_by_order_id = defaultdict(list)
-            for x, y in orders_by_volume[arbitrage_id]:
+            for x, y in orders_by_arbitrage_id[arbitrage_id]:
                 trades_by_order_id[x.deal_id].append( (x,y) )
 
             for x in trades_by_order_id[arbitrage_id]:
                 if len(trades_by_order_id[x])!=1:
                     raise
 
-            for order1, trade_list in orders_by_volume[arbitrage_id]:
+            for order1, trade_list in orders_by_arbitrage_id[arbitrage_id]:
                 order_pair = find_candidates(order1, list_of_orders)
                 if order_pair is not None:
                     xxxxxxx, order_pair_trades = trades_by_order_id[order_pair.deal_id][0]
@@ -200,7 +124,7 @@ def compute_profit_by_pair(trades_to_order_by_pair):
                 else:
                     msg = "can't find pair order - {o}".format(o=order1)
                     log_to_file(msg, "missing_" + get_pair_name_by_id(order1.pair_id) + ".txt")
-                    cnt += 1
+                    number_of_missing_pair += 1
 
             for grouping_id in deal_ids:
                 if len(deal_ids[grouping_id])!=2:
@@ -229,8 +153,8 @@ def compute_profit_by_pair(trades_to_order_by_pair):
                                                     """.format(d1=deal_2, d2=deal_1, cp=current_profit)
                         log_to_file(msg, "arbitrage_log_" + get_pair_name_by_id(deal_1.pair_id) + ".txt")
         else:
-            deal_1, deal_1_trades = orders_by_volume[arbitrage_id][0]
-            deal_2, deal_2_trades = orders_by_volume[arbitrage_id][1]
+            deal_1, deal_1_trades = orders_by_arbitrage_id[arbitrage_id][0]
+            deal_2, deal_2_trades = orders_by_arbitrage_id[arbitrage_id][1]
 
             if deal_1.trade_type == DEAL_TYPE.SELL:
                 current_profit = compute_profit_by_arbitrage(deal_1, deal_1_trades, deal_2, deal_2_trades)
@@ -249,9 +173,49 @@ def compute_profit_by_pair(trades_to_order_by_pair):
                                 """.format(d1=deal_2, d2=deal_1, cp=current_profit)
                 log_to_file(msg, "arbitrage_log_" + get_pair_name_by_id(deal_1.pair_id) + ".txt")
 
-    print cnt
+    print "Number of missing pair", number_of_missing_pair
 
     return profit_by_pair
+
+
+def compute_loss(orders_by_arbitrage_id):
+
+    orders_by_pair = defaultdict(list)
+
+    cnt = 0
+    for arbitrage_id in orders_by_arbitrage_id:
+        if len(orders_by_arbitrage_id[arbitrage_id]) != 1:
+            continue
+        order, trades_list = orders_by_arbitrage_id[arbitrage_id]
+        msg = "can't find pair order - {o}".format(o=order)
+        log_to_file(msg, "missing_" + get_pair_name_by_id(order.pair_id) + ".txt")
+        cnt += 1
+        orders_by_pair[order.pair_id].append( (order, trades_list) )
+
+    loss_by_coin = Counter()
+    loss_by_coin_bitcoin = Counter()
+
+    for pair_id in orders_by_pair:
+        loss_by_coin[pair_id], loss_by_coin_bitcoin[pair_id] = compute_loss_by_pair(orders_by_pair[pair_id])
+
+    return loss_by_coin, loss_by_coin_bitcoin
+
+
+def compute_loss_by_pair(orders_and_trades_by_pair):
+    volume_pair = 0.0
+    volume_bitcoin = 0.0
+
+    for order, trade_list in orders_and_trades_by_pair:
+        if order.trade_type == DEAL_TYPE.BUY:
+            for trade in trade_list:
+                volume_pair += trade.volume
+                volume_bitcoin -= trade.volume * trade.price
+        elif order.trade_type == DEAL_TYPE.SELL:
+            for trade in trade_list:
+                volume_pair -= trade.volume
+                volume_bitcoin += trade.volume * trade.price
+
+    return volume_pair, volume_bitcoin
 
 
 if __name__ == "__main__":
@@ -356,3 +320,11 @@ if __name__ == "__main__":
 
     for x in failed_deal_ids:
         log_to_file(x, "NULL_deal_id.log")
+
+    loss_by_coin, loss_by_coin_bitcoin = compute_loss(orders_with_corresponding_trades)
+    for pair_id in loss_by_coin_bitcoin:
+        pair_name = get_pair_name_by_id(pair_id)
+        log_to_file("{pn} - Volume change {p} Bitcoin volume change {vp}".format(pn=pair_name,
+                                                                                 p=float_to_str(loss_by_coin[pair_id]),
+                                                                                 vp=float_to_str(loss_by_coin_bitcoin[pair_id])),
+                    "what_we_have_at_the_end.log")
