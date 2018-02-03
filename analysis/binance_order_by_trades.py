59f496ae890e10005ad87daab6ef197b89591e60
EC2 Default User <ec2-user@ip-172-31-31-75.us-west-1.compute.internal>
2018-02-03 08:00:36 +0000

    Update logic to generate profit report with presence of manual orders at binance

--- analysis/binance_order_by_trades.py
+++ analysis/binance_order_by_trades.py
@@ -2,6 +2,7 @@ from collections import Counter
 
 from utils.file_utils import log_to_file
 from utils.currency_utils import get_pair_name_by_id
+from utils.string_utils import float_to_str
 
 
 def group_binance_trades_per_order(binance_orders_at_exchange, binance_trades_group_by_pair, binance_orders_at_bot):
@@ -16,10 +17,10 @@ def group_binance_trades_per_order(binance_orders_at_exchange, binance_trades_gr
         # if order.pair_id != 41:
         #    continue
 
-        # msg = """STARTED for order - {wtf}
-        #   LAST_ID is {lid}
-        #    """.format(wtf=order, lid=last_trade_idx[order.pair_id])
-        #log_to_file(msg, get_pair_name_by_id(order.pair_id) + ".txt")
+        msg = """STARTED for order - {wtf}
+        LAST_ID is {lid}
+        """.format(wtf=order, lid=last_trade_idx[order.pair_id])
+        log_to_file(msg, get_pair_name_by_id(order.pair_id) + ".txt")
 
 
         list_to_check = binance_trades_group_by_pair[order.pair_id][last_trade_idx[order.pair_id]:]
@@ -32,40 +33,53 @@ def group_binance_trades_per_order(binance_orders_at_exchange, binance_trades_gr
             log_to_file(msg, get_pair_name_by_id(order.pair_id) + ".txt")
             for twt in binance_trades_group_by_pair[order.pair_id]:
                 log_to_file(twt, get_pair_name_by_id(order.pair_id) + ".txt")
-
+            
             raise
 
         total_volume = float(0.0)
         cur_trade_list = []
 
         for trade in list_to_check:
-            if abs(order.executed_volume - total_volume) > 0.00000001:
+            
+            if trade.create_time < order.create_time:
+                # In case SOMEONE put manual orders we have to take it into account
+                last_trade_idx[order.pair_id] += 1
+                msg = "SKIPPING TRADE BECAUSE OF TIME - {ttt}".format(ttt=trade)
+                log_to_file(msg, get_pair_name_by_id(order.pair_id) + ".txt")
+
+                continue 
+
+            if abs(order.executed_volume - total_volume) > 0:
+
+                last_trade_idx[order.pair_id] += 1
 
                 total_volume += trade.volume
                 cur_trade_list.append(trade)
-                last_trade_idx[order.pair_id] += 1
 
-                # msg = """ADD NEW TRADE - {o}
-                #    LAST_ID NOW IS {lid}
-                #    total_volume = {tv}
-                #    order.executed_volume = {ev}
-                #    WTF = {wtf}
-                #    DIFF: {wtf1}
-                #    """.format(o=trade, lid=last_trade_idx[order.pair_id], tv=total_volume, ev=order.executed_volume,
-                #               wtf=order.executed_volume > total_volume, wtf1=order.executed_volume - total_volume)
-                #log_to_file(msg, get_pair_name_by_id(order.pair_id) + ".txt")
+                msg = """ADD NEW TRADE - {o}
+                    LAST_ID NOW IS {lid}
+                    total_volume = {tv}
+                    order.executed_volume = {ev}
+                    WTF = {wtf}
+                    DIFF: {wtf1}
+                    """.format(o=trade, lid=last_trade_idx[order.pair_id], tv=total_volume, ev=order.executed_volume,
+                               wtf=order.executed_volume > total_volume, wtf1=order.executed_volume - total_volume)
+                log_to_file(msg, get_pair_name_by_id(order.pair_id) + ".txt")
 
                 if total_volume - order.executed_volume > 0.000001:
                     msg = """current order: {o}
                                     current last_id - {lid}
-                                    PREV list:""".format(o=order, lid=last_trade_idx[order.pair_id])
+                                    volume_diff: {vd}
+                                    PREV list:""".format(o=order, lid=last_trade_idx[order.pair_id],vd=float_to_str(total_volume - order.executed_volume))
                     log_to_file(msg, get_pair_name_by_id(order.pair_id) + ".txt")
                     for twt in binance_trades_group_by_pair[order.pair_id]:
                         log_to_file(twt, get_pair_name_by_id(order.pair_id) + ".txt")
                     print "YOUR PROBLEM IS ", order.pair_id, get_pair_name_by_id(order.pair_id)
                     raise
-            else:
-                # log_to_file("SHOULD BE ENOUGH VOLUME", get_pair_name_by_id(order.pair_id) + ".txt")
+
+            # floating point arithmetic in python behind good and bad
+            if abs(order.executed_volume - total_volume) <= 0.00000001:
+                log_to_file("SHOULD BE ENOUGH VOLUME", get_pair_name_by_id(order.pair_id) + ".txt")
                 break
 
         # msg = """Added succesfully: {o}
