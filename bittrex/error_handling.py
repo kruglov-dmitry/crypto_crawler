a985e0c915c25a02445d68574bb4075ef3daa8a4
Dmitry <kruglov.dima@gmail.com>
2018-03-18 21:23:44 +0400

    #45 update error checking methods for bittrex

--- bittrex/error_handling.py
+++ bittrex/error_handling.py
@@ -5,7 +5,11 @@ def is_error(json_document):
     :return:
     """
 
-    if json_document is None or "result" not in json_document or json_document["result"] is None:
+    if json_document is None or "result" not in json_document:
+        return True
+
+    if (json_document["result"] is None and "success" not in json_document) or \
+            ("success" in json_document and json_document["success"] is False):
         return True
 
     return False
