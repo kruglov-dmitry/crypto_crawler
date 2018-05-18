584732693a99fa2aae23fe77e927086c337a76fd
Kruglov Dmitry <kruglovdmitry@MacBook-Pro-Kruglov.local>
2018-05-18 07:27:50 +0400

    #45 main puppeter added

---
+++ deploy/system_utils.py
@@ -0,0 +1,1 @@
+def stop_process(pid):
---
+++ services/node_puppeteer.py
@@ -0,0 +1,43 @@
+import socket
+import argparse
+from utils.time_utils import sleep_for
+from deploy.classes.CommonSettings import CommonSettings
+from data_access.classes.CommandQueue import CommandQueue
+
+import subprocess32 as subprocess
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="""
+    Core management unit responsible for re-spawning arbitrage process and updating configuration
+    """)
+
+    parser.add_argument('--cfg', action='store', required=True)
+    arguments = parser.parse_args()
+
+    settings = CommonSettings.from_cfg(arguments.cfg)
+
+    command_queue = CommandQueue(settings.cache_host, settings.cache_port)
+
+    #
+    # whilte true read settings
+    #   1) what kind of services to deploy
+    #   2) service specific settings
+
+    server_name = socket.gethostname()
+    command_queue.register_node(server_name)
+
+    # p = r.pubsub()
+    # p.subscribe('test')
+
+
+    puppets = []
+
+    while True:
+        # get command
+        # execute
+
+        cmd = command_queue.get_command()
+        if cmd:
+            print "Subscriber: %s" % message['data']
+        sleep_for(1)
