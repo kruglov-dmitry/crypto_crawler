2d31eb2f862430e8739e8017f8488d452b618ca9
Kruglov Dmitry <kruglovdmitry@MacBook-Pro-Kruglov.local>
2018-07-24 09:21:32 +0300

    #74 add modified test for insertion validation of ask bids

--- test_asc.py
+++ test_asc.py
@@ -1,104 +1,78 @@
-class Deal:
-    def __init__(self, price, volume):
-        self.price = price
-        self.volume = volume
-
-    def __str__(self):
-        attr_list = [a for a in dir(self) if not a.startswith('__') and not callable(getattr(self, a))]
-        str_repr = "["
-        for every_attr in attr_list:
-            str_repr += every_attr + " - " + str(getattr(self, every_attr)) + " "
-
-        str_repr += "]"
-
-        return str_repr
-
-    def __eq__(self, other):
-        print "EQ"
-        """Overrides the default implementation"""
-        if isinstance(self, other.__class__):
-            # FIXME ROUNDING
-            return self.price == other.price
-        return False
-
-    def __lt__(self, other):
-        """
-        Used by biset.insert to maintain ordered asks\bids of order book
-        :param other:
-        :return:
-        """
-        print "comparing", self.price, "and", other.price, " result", self.price < other.price   
-        return self.price > other.price
-
-def search(some_list, target, cmp_method):
-    min_idx = 0
-    max_idx = len(some_list) - 1
-    mid_idx = (min_idx + max_idx) / 2
-
-    # uncomment next line for traces
-    print "Elem for insertion: ", target, mid_idx
-    print "Current list:"
-    for b in some_list:
-        print b
-
-    if mid_idx < 0:
-        return 0 
-    elif min_idx == max_idx: 
-        # i.e. single element
-        if cmp_method(some_list[mid_idx], target):
-            return mid_idx + 1
-        else:
-            return mid_idx
-
-    while min_idx < max_idx:
-        print mid_idx
-        if some_list[mid_idx].price == target.price:
-        # if some_list[mid_idx] == target:
-            print "return"
-            return mid_idx
-        elif cmp_method(some_list[mid_idx], target):
-            print "Go deeper: ", mid_idx + 1
-            return mid_idx + 1 + search(some_list[mid_idx + 1:], target, cmp_method)
-        else:
-            print "Back"
-            return search(some_list[:mid_idx], target, cmp_method)
-
-def cmp_method_bid(a, b):
-    return a.price < b.price
-
-def cmp_method_ask(a, b):
-    return a.price > b.price
+from analysis.binary_search import binary_search
+from data.Deal import Deal
+from data.OrderBook import cmp_method_ask, cmp_method_bid
 
 import random
 
 a = []
-for idx in xrange(10):
+# for idx in xrange(10):
+#  b = Deal(random.uniform(0, 1), random.uniform(0, 1))
+#  a.append(b)
 
-  b = Deal(random.uniform(0, 1), random.uniform(0, 1))
-  a.append(b)
+a.append(Deal(0.03172795, random.uniform(0, 1)))
+a.append(Deal(0.03172796, random.uniform(0, 1)))
+a.append(Deal(0.03172798, random.uniform(0, 1)))
+a.append(Deal(0.03172801, random.uniform(0, 1)))
+a.append(Deal(0.03173, random.uniform(0, 1)))
+a.append(Deal(0.03174392, random.uniform(0, 1)))
+a.append(Deal(0.0318111, random.uniform(0, 1)))
+a.append(Deal(0.03189575, random.uniform(0, 1)))
+a.append(Deal(0.03197009, random.uniform(0, 1)))
+a.append(Deal(0.03197015, random.uniform(0, 1)))
 
-a = sorted(a, key=lambda x: x.price, reverse=True)
-# a = sorted(a, key=lambda x: x.price, reverse=False)
+a = sorted(a, key=lambda x: x.price, reverse=False)
 
 print "Before insertion: "
 for aa in a:
   print aa
 
-b = Deal(random.uniform(0, 1), random.uniform(0, 1))
-b1 = Deal(random.uniform(0, 1), random.uniform(0, 1))
+# b = Deal(0.03172794, 100400.0)
+b = Deal(0.03172795, 100400.0)
 
-import bisect
+# smaller_case = Deal(0.000001, random.uniform(0, 1))
 
-# bisect.insort_left(a, b)
-# bisect.insort_left(a, b1)
-idx = search(a, b, cmp_method_ask)
+# FIND and delete it
+# b2 = Deal(0.03835944, random.uniform(0, 1))
+
+# FIND and update volume
+# b3 = Deal(0.03835945, 10)
+
+idx = binary_search(a, b, cmp_method_ask)
 print "Found index for insertion: ",  idx, b
-a.insert(idx, b)
-idx = search(a, b1, cmp_method_ask)
-print "Found index for insertion: ",  idx, b1
-a.insert(idx, b1)
 
+item_insert_point = binary_search(a, b, cmp_method_ask)
+is_present = False
+if item_insert_point < len(a):
+    is_present = a[item_insert_point] == b
+
+if is_present:
+    a[item_insert_point].volume = b.volume
+else:
+    a.insert(idx, b)
+
+# idx = binary_search(a, b2, cmp_method_ask)
+# idx = search(a, b2, cmp_method_ask)
+# print "Found index for deletion: ",  idx, b2
+# a.insert(idx, b1)
+# del a[idx]
+
+# idx = search(a, b3, cmp_method_ask)
+# idx = binary_search(a, b3, cmp_method_ask)
+# print "Found index for update: ",  idx, b3
+# a[idx].volume += b3.volume
 
 print "After insertion: "
 for aa in a:
   print aa
+
+"""
+idx = binary_search(a, smaller_case, cmp_method_ask)
+# idx = search(a, b2, cmp_method_ask)
+print "Found index for insertion in front of array: ",  idx, smaller_case
+a.insert(idx, smaller_case)
+
+
+print "After final insertion: "
+for aa in a:
+  print aa
+"""
